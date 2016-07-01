c26fcf0414f9efe1458ca8776a9884ad2b4ee73c
Oisin Smith <oisin@oisinsmith.eu>
2016-07-01 17:52:52 +0200

    Add Logging

--- VertretungPlan.py
+++ VertretungPlan.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
-
 #
 # (c) 2016 Oisín Smith All Rights Reserved
 #
 
 import re
 import json
+import logging
 from bs4 import BeautifulSoup
 from upload import Uploader
 
 OUTPUT_FILE = "vt.json"
+LOG_FILE = "latest.log"
+
+logging.basicConfig(filename=LOG_FILE,
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s: %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 
 def main():
@@ -22,31 +29,34 @@ def main():
 
     output = {}
 
-    for row in table.find_all("tr")[2:-2]:
+    table = table.find_all("tr")[2:-2]
+    for row in table:
         changes = {}
         column = row.td
         sub = str(column.li)
         cls = column.h3.contents[0]
 
-        if cls in ["AG"]:
+        if cls in ["AG", "1. Semester", "2. Semester", "4. Semester", "4. Semester", "Nachschreiber", "Aufsicht"]:
             continue
-        # print("Class: " + str(cls))
 
         for res in r.findall(sub):
-            key = str(res[0])
+            change = [res[1], (res[2] or res[3]).strip()]
+            changes[str(res[0])] = change
 
-            change = [res[1], res[2] or res[3]]
-            change[1] = change[1].strip()
-            changes[key] = change
+        output.setdefault(cls, []).append(changes)
 
-        if cls not in output.keys():
-            output[cls] = []
-        output[cls].append(changes)
-
+    logging.info("Found " + str(len(table)) + " Items")
     with open(OUTPUT_FILE, "w") as file:
         file.write(json.dumps(output))
 
-    Uploader("", OUTPUT_FILE, "")
+    u = Uploader(OUTPUT_FILE, None, "http://www.oisinsmith.eu/Uploader/upload.php")
+    u.send_post()
 
 if __name__ == "__main__":
-    main()
+    try:
+        logging.info("Program Started")
+        main()
+        logging.info("Finished\n")
+    except BaseException as e:
+        logging.error(repr(e))
+        raise e
